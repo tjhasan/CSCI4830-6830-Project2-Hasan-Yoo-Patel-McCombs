6febb24093e0405f0bcbb46b3e17c1058560d368
TJ Hasan <tjhasan159@gmail.com>
2019-03-23 16:52:24 -0400

    Fixed the force exit position reset

--- Project2-Hasan-Yoo-Patel-McCombs/Assets/Elevator_Engine.cs
+++ Project2-Hasan-Yoo-Patel-McCombs/Assets/Elevator_Engine.cs
@@ -5,41 +5,52 @@ using UnityEngine;
 public class Elevator_Engine : MonoBehaviour
 {
     private float speed = 0;
-    private Vector3 startPosition;
+    private int level = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Transform startPosition = transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {//Have 4 levels of speed. 0 1 2 and 3, with 3 being the highest. Allow therapist to use arrow keys to also control speed.
         //Below code used for the movement of the elevator
+
         transform.position += Vector3.up * speed * Time.deltaTime;
 
-        //speeds not final
-        if (Input.GetKeyDown(KeyCode.A))//replace keyboard keys with in game physical buttons. Arrow buttons allowed for therapist.
-        {//level 0
+        if (level == 0)
+        {//level 1
             speed = 0;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (level == 1)
+        {//level 1
+            speed = 2;
+        }
+
+        if (level == 2)
         {//level 1
             speed = 5;
         }
 
-        //the following speeds still need testing.
+        if (level == 3)
+        {//level 1
+            speed = 10;
+        }
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{//level 2
-        //    speed = 5;
-        //}
+        //speeds not final
+        if (Input.GetKeyDown(KeyCode.UpArrow) && level < 3)//replace keyboard keys with in game physical buttons. Arrow buttons allowed for therapist.
+        {
+            level++;
+            Debug.Log(level);
+        }
 
-        //if (Input.GetKeyDown(KeyCode.0))
-        //{//level 3
-        //    speed = 5;
-        //}
+        if (Input.GetKeyDown(KeyCode.DownArrow) && level > 0)
+        {//level 1
+            level--;
+            Debug.Log(level);
+        }
 
         //Following code is used for the force stop of the session:
         //if(OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) &&
@@ -52,8 +63,7 @@ public class Elevator_Engine : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            speed = 0;
-            transform.position = startPosition;
+            transform.position = new Vector3(1.111f, 0.824f, 0.573f);
         }
     }
 }
