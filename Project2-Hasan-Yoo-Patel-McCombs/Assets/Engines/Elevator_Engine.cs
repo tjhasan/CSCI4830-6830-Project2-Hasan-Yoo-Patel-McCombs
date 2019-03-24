c73736ad52cf7649f25cc1a74365e986aa2b8484
TJ Hasan <tjhasan159@gmail.com>
2019-03-23 21:33:01 -0400

    Removed physical buttons. Added analog buttons for speed ups
    Physical buttons were not working well with the moving elevator. Not speed is decided based on the number of times you click "A" going up to a ceiling of 3. Goes down 1 level for every click of "X" with a floor of 0.

--- Project2-Hasan-Yoo-Patel-McCombs/Assets/Engines/Elevator_Engine.cs
+++ Project2-Hasan-Yoo-Patel-McCombs/Assets/Engines/Elevator_Engine.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Elevator_Engine : MonoBehaviour
 {
     public float speed = 0;
-    private int level = 0;
+    public int level = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,29 +25,31 @@ public class Elevator_Engine : MonoBehaviour
         }
 
         if (level == 1)
-        {//level 1
+        {//level 2
             speed = 2;
         }
 
         if (level == 2)
-        {//level 1
+        {//level 3
             speed = 5;
         }
 
         if (level == 3)
-        {//level 1
+        {//level 4
             speed = 10;
         }
 
         //speeds not final
-        if (Input.GetKeyDown(KeyCode.UpArrow) && level < 3)//replace keyboard keys with in game physical buttons. Arrow buttons allowed for therapist.
+        if (Input.GetKeyDown(KeyCode.UpArrow) && level < 3 ||//replace keyboard keys with in game physical buttons. Arrow buttons allowed for therapist.
+            OVRInput.GetDown(OVRInput.Button.One) && level < 3)
         {
             level++;
             Debug.Log(level);
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) && level > 0)
-        {//level 1
+        if (Input.GetKeyDown(KeyCode.DownArrow) && level > 0 ||
+            OVRInput.GetDown(OVRInput.Button.Three) && level > 0)
+        {
             level--;
             Debug.Log(level);
         }
@@ -58,11 +60,13 @@ public class Elevator_Engine : MonoBehaviour
             OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger) &&
             OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
         {
+                level = 0;
                 transform.position = new Vector3(1.111f, 0.824f, 0.573f);
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
+            level = 0;
             transform.position = new Vector3(1.111f, 0.824f, 0.573f);
         }
     }
