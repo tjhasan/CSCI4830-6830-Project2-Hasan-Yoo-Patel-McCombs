870ceedd440d9209ad3b7768e0af82eb4b6315c7
Arth <arth0505@gmail.com>
2019-03-28 18:55:05 -0400

    Created Rotating Elevator door and Shrunk size of Extreme Level

---
+++ Project2-Hasan-Yoo-Patel-McCombs/Assets/Engines/DoorEngine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorEngine : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    public GameObject Door;
+    public GameObject Elevator;
+    private bool isTurn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Reach height of plank
+        if(Elevator.transform.position.y >= 136)
+        {
+            if (!isTurn)
+            {
+                Door.transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+                if(Door.transform.rotation.y >= 0.0f)
+                {
+                    isTurn = true;
+                }
+            }
+        }
+        else
+        {
+            Door.transform.Rotate(Vector3.forward,0.0f);
+        }
+    }
+
+
+}
