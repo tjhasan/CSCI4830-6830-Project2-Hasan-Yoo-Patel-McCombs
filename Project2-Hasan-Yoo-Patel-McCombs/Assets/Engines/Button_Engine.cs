bde25deb576eab60ded35eacc8d619d0d2f0c2d3
TJ Hasan <tjhasan159@gmail.com>
2019-03-29 16:41:17 -0400

    Added physical buttons to the elevator.

---
+++ Project2-Hasan-Yoo-Patel-McCombs/Assets/Engines/Button_Engine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Engine : MonoBehaviour
+{
+    public Elevator_Engine ee;
+    public GameObject upButton;
+    public GameObject stopButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == upButton.name)
+        {
+            ee.level = 1;
+        }
+        if (collision.gameObject.name == stopButton.name)
+        {
+            ee.level = 0;
+        }
+    }
+}
