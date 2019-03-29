886f11b2f12e9c3342bb92285e379f436e5ddb1c
Jordan <soccereagle935@aol.com>
2019-03-29 12:32:16 -0400

    Updates Level TIme on screen
    Added counting level time of levels, will be adding on pick up gun too later

--- Project2-Hasan-Yoo-Patel-McCombs/Assets/Scripts/GameManager.cs
+++ Project2-Hasan-Yoo-Patel-McCombs/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -33,8 +34,15 @@ public class GameManager : MonoBehaviour
     bool level1Played = false;
     bool level2Played = false;
     bool level3Played = false;
+    bool levelExtPlayed = false;
 
-
+    public TextMeshProUGUI gameText;
+    float levelTime;
+    float levelTimeLvl1;
+    float levelTimeLvl2;
+    float levelTimelvl3;
+    float extremeLevelTime;
+    bool startTime = false;
 
 
 
@@ -66,7 +74,7 @@ public class GameManager : MonoBehaviour
     void fire()
 
     {
-Debug.Log("fired");
+        Debug.Log("fired");
         Bullet b = Instantiate<Bullet>(bulletPrefab, bulletLoc.transform.position, bulletLoc.transform.rotation);
 
         Destroy(b.gameObject, 1.0f);
@@ -77,22 +85,23 @@ Debug.Log("fired");
     void miniGame()
     {
         Debug.Log("inMini");
-        
-        
 
-         if (level == 1 || level ==2 || level == 3)
+
+
+        if (level == 1 || level == 2 || level == 3)
         {
             Debug.Log("in level");
-           // Instantiate(textLvl1, textLvl1Loc.transform.position, textLvl1Loc.transform.rotation);
+            // Instantiate(textLvl1, textLvl1Loc.transform.position, textLvl1Loc.transform.rotation);
             elv.level = 0;
             Vector3[] targetPos = new[] { new Vector3(-104.7494f, -0.2f, 155.03f), new Vector3(-104.7494f, -0.2f, 144.16f), new Vector3(-117.01f, -0.2f, 148.62f), new Vector3(-90.99f, -0.2f, 145.42f), new Vector3(-90.99f, -0.2f, 154.09f) };
             for (int i = 0; i < 5; i++)
             {
                 targetCount++;
                 Instantiate(target, targetPos[i], Quaternion.identity);
-Debug.Log("created target");
+                Debug.Log("created target");
             }
-            // while (targetCount != 0)
+
+            // while (targetCount != 0)  //if beat stop time and set levelTime to 0
             // {
             //     elv.level = 0;
             // }
@@ -102,9 +111,40 @@ Debug.Log("created target");
 
     // Update is called once per frame
 
-    void Update()
+    void Update() {
 
-    {
+        if (level==1) //&&gunpcikedup
+        {
+            startTime = true;
+        }
+        if (startTime)
+        {
+            if (level1Played == false)
+            {
+                levelTime += Time.deltaTime;
+                gameText.text = "Name:      [Name writing TBD]\nLevel 1:    [" + levelTime + "]\nLevel 2:    [Timer TBD]\nLevel 3:    [Timer TBD]\nExtreme:  [Timer TBD] ";
+            }
+
+            else if(level1Played==true && level1Played == false)
+            {
+                levelTime += Time.deltaTime;
+                gameText.text = "Name:      [Name writing TBD]\nLevel 1:    [" + levelTimeLvl1 + "]\nLevel 2:    ["+levelTime+"]\nLevel 3:    [Timer TBD]\nExtreme:  [Timer TBD] ";
+            }
+
+            else if (level2Played == true && level3Played==false)
+            {
+                levelTime += Time.deltaTime;
+                gameText.text = "Name:      [Name writing TBD]\nLevel 1:    [" + levelTimeLvl1 + "]\nLevel 2:    ["+ levelTimeLvl2 + "]\nLevel 3:    ["+levelTime+"]\nExtreme:  [Timer TBD] ";
+            }
+
+            else if(levelExtPlayed == false)
+            {
+                levelTime += Time.deltaTime;
+                gameText.text = "Name:      [Name writing TBD]\nLevel 1:    [" + levelTimeLvl1 + "]\nLevel 2:    [" + levelTimeLvl2 + "]\nLevel 3:    [" + levelTimelvl3 + "]\nExtreme:  ["+extremeLevelTime+"] ";
+            }
+            
+
+        }
      if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
             {
                 Debug.Log("gunHeld");
@@ -116,8 +156,6 @@ Debug.Log("created target");
             }
 
         }  
-        // Debug.Log("Elevator" + Elevator.transform.position.y);
-        //Debug.Log("origin" + originElv.y);
         if (Elevator.transform.position.y >= originElv.y + 10f && level1Played==false)
         {
             level = 1;
