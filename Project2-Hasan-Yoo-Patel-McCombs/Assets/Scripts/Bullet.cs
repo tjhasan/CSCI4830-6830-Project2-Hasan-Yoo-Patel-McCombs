407d0264ef54ef8e9d7d8056fb8b340a64836dcc
TJ Hasan <tjhasan159@gmail.com>
2019-03-30 17:14:09 -0400

    Targets now disappear when hit y a bullet
    Elevator still needs to be able to move after the level is beaten.

--- Project2-Hasan-Yoo-Patel-McCombs/Assets/Scripts/Bullet.cs
+++ Project2-Hasan-Yoo-Patel-McCombs/Assets/Scripts/Bullet.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-	// Start is called before the first frame update
-	void Start()
-	{
+    public GameObject target;
+    public float bulletLifeTime;
+    public float bulletSpeed;
 
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
 
-	// Update is called once per frame
-	void Update()
-	{
-		transform.position = transform.position + .5f * transform.forward;
-		//transform.Translate(0,0,.5f);
-	}
+    }
 
-	private void OnCollisionEnter(Collision collision)
-	{
-	/* 	if (collision.gameObject.name != "Gun" && collision.gameObject.name == "Target")
-		{
-			Destroy(this.gameObject);
+    // Update is called once per frame
+    void Update()
+    {
+        bulletLifeTime -= Time.deltaTime;
+        if (bulletLifeTime < 0)
+        {
+            Debug.Log("Destroying Rocket");
+            Destroy(this.gameObject);
+        }
+        transform.Translate(0, 0, bulletSpeed);
+        transform.position = transform.position + .5f * transform.forward;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Target(Clone)")
+        {
             Destroy(collision.gameObject);
-		}*/
-	}
+            Destroy(this.gameObject);
+            GameManager.targetCount--;
+        }
+    }
 }
