cb62dc110414039f85474207a8e616417b05afca
Arth <arth0505@gmail.com>
2019-04-02 13:15:41 -0400

    Fixed Survey Scene
    Added methods to extract values from the survey and save them.

---
+++ Project2-Hasan-Yoo-Patel-McCombs/Assets/Engines/SurveyEngine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurveyEngine : MonoBehaviour
+{
+    int experienceRating;
+    int anxietyRating;
+    string AnxietyLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SliderOneValue(int val)
+    {
+        experienceRating = val;
+    }
+
+    public void SliderTwoValue(int val)
+    {
+        anxietyRating = val;
+    }
+
+    public void ImprovedButtonClicked()
+    {
+        AnxietyLevel = "Improved";
+    }
+
+    public void WorsenedButtonClicked()
+    {
+        AnxietyLevel = "Worsened";
+    }
+
+    public void SubmitButtonPressed()
+    {
+        PlayerPrefs.SetInt("experienceRating", experienceRating);
+        PlayerPrefs.SetInt("anxietyRating", anxietyRating);
+        PlayerPrefs.SetString("AnxietyLevel", AnxietyLevel);
+    }
+}
